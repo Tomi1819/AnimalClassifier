6d7509c81da0ad3f8fd2f9c8916ecd725a1a14e3
Tomi1819 <toshevtomi@gmail.com>
2025-02-27 14:35:56 +0200

    Database implemented

---
+++ AnimalClassifier/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,16 @@
+﻿namespace AnimalClassifier.Extensions
+{
+    using AnimalClassifier.Infrastructure.Data;
+    using Microsoft.EntityFrameworkCore;
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddApplicationDbContex(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+            services.AddDbContext<AnimalClassifierDbContext>(options =>
+                options.UseSqlServer(connectionString));
+            return services;
+        }
+    }
+}
