72e05337bd359de6aa44b23b8738e20e308abae3
Tomi1819 <toshevtomi@gmail.com>
2025-05-16 16:19:16 +0300

    video path added to the respond

--- AnimalClassifier.Core/DTO/VideoUploadResult.cs
+++ AnimalClassifier.Core/DTO/VideoUploadResult.cs
@@ -4,5 +4,6 @@
     {
         public int FramesProcessed { get; set; }
         public List<AnimalSummary> TopAnimals { get; set; } = new();
+        public string VideoPath { get; set; } = string.Empty;
     }
 }
