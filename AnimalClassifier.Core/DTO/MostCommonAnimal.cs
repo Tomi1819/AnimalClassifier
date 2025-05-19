c23a167c740c34fede97e2f52e85b187f57255a4
Tomi1819 <toshevtomi@gmail.com>
2025-05-19 16:23:51 +0300

    DTO added

---
+++ AnimalClassifier.Core/DTO/MostCommonAnimal.cs
@@ -0,0 +1,8 @@
+﻿namespace AnimalClassifier.Core.DTO
+{
+    public class MostCommonAnimal
+    {
+        public string AnimalName { get; set; } = string.Empty;
+        public int Count { get; set; }
+    }
+}
