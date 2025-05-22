aa8d8cc33fb6c35ca9a117ca616a82bd5b62f30a
Tomi1819 <toshevtomi@gmail.com>
2025-05-22 13:17:40 +0300

    Basic search functionality implemented

---
+++ AnimalClassifier.Core/Contracts/IAnimalService.cs
@@ -0,0 +1,8 @@
+﻿namespace AnimalClassifier.Core.Contracts
+{
+    using AnimalClassifier.Core.DTO;
+    public interface IAnimalService
+    {
+        Task<List<AnimalSearchResult>> SearchAnimalByNameAsync(string searchTerm);
+    }
+}
