05e9613434243935fcb510d29c4edd6696cdd0fc
Tomi1819 <toshevtomi@gmail.com>
2025-05-22 13:15:19 +0300

    Constants added

---
+++ AnimalClassifier/Constants/MessageConstants.cs
@@ -0,0 +1,14 @@
+﻿namespace AnimalClassifier.Constants
+{
+    public static class MessageConstants
+    {
+        //AnimalController
+        public const string EnterSearchTerm = "Please, enter a search term.";
+        public const string NoMatches = "There is no mathes yet.";
+
+        //ServiceCollectionExtension
+        public const string MissingConnectionString = "Connection string 'DefaultConnection' not found.";
+        public const string MissingMLModelPath = "ML model path is not configured.";
+        public const string MissingJwtSecurityKey = "JWT Secret Key is not configured.";
+    }
+}
