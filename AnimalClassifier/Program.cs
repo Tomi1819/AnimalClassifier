02864825cab68add9758c3e1388f38d100e5cd37
Tomi Toshev <tomi.toshev@eveliko.com>
2026-09-06 13:07:12 +0300

    Removed swagger UI - it's never used.

--- AnimalClassifier/Program.cs
+++ AnimalClassifier/Program.cs
@@ -9,25 +9,11 @@ builder.Services.AddApplicationIdentity(builder.Configuration);
 
 builder.Services.AddControllers();
 
-builder.Services.AddEndpointsApiExplorer();
-
 builder.Services.AddApplicationServices(builder.Configuration, builder.Environment);
 
 builder.Services.AddApplicationCors(builder.Configuration);
 
-builder.Services.AddSwaggerGen(c =>
-{
-    c.EnableAnnotations();
-});
-
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 
